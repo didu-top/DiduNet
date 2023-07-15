f5198e6a15d6ebb18e0e60bf98ca0796b322c71a
miao <611161512@qq.com>
2023-07-16 00:37:07 +0800

    MOD: 调整缩进

--- Sources/DiduNet/Core/Network.swift
+++ Sources/DiduNet/Core/Network.swift
@@ -14,236 +14,236 @@ import DiduFoundation
 
 /// 捕获之后的动作
 public enum ActionAfterCatch {
-    /// 标记为取消
-    case markCancel
-    /// 转移请求
-    case transferred
-    /// 继续执行
-    case `continue`
+  /// 标记为取消
+  case markCancel
+  /// 转移请求
+  case transferred
+  /// 继续执行
+  case `continue`
 }
 
 public typealias NetworkCallback<T> = (DNResult<T>) -> Void
 public typealias CatchCallback = (ActionAfterCatch) ->Void
 
 internal let logger = Logger(lowerLevel: .verbose, prefixMap: [
-    .verbose: "􀤆􀤆 - 🗒 => ",
-    .warn: "􀤆􀤆 - ⚠️ => ",
-    .error: "􀤆􀤆 - ❌ => "
+  .verbose: "􀤆􀤆 - 🗒 => ",
+  .warn: "􀤆􀤆 - ⚠️ => ",
+  .error: "􀤆􀤆 - ❌ => "
 ])
 
 
 // MARK: - 网络请求（返回原始数据）
 public class Network {
+  
+  /// 需要捕获的HTTP错误码列表，比如: [404,401]
+  public static var globalCatchHttpCodeList: [Int] = []
+  
+  /// 设置全局捕获HTTP错误码的动作， 闭包参数为：( API, 状态码，继续执行的回调: 是否将任务标记为取消)
+  public static var globalCatchHttpErrorCodeAction: ((CachableTarget, Int, @escaping NetworkCallback<Response>, CatchCallback) -> Void)?
+  
+  /// 设置判定领域设计(即服务器接口数据设计)请求成功的设计标记
+  public static var domainSucessKeyValePair: (String, DNDomainCode) = ("code", DNDomainCode(stringValue: "C0000"))
+  
+  public static var domainFailedMessageKey: String = "message"
+  
+  /// 设置全局捕获领域设计(即服务器接口数据设计)错误码的动作，闭包参数为：( API, 响应原始数据，需要继续执行的回调: 是否将任务标记为取消)
+  public static var domainMiddlewareAction: ((CachableTarget, Data, @escaping NetworkCallback<Response>, CatchCallback) -> Void)?
+  
+  /// 是否全局启用日志
+  public static var isEnableLog = false
+  
+  /// 默认请求超时
+  public static var defaultTimeOut: Double = 30
+  
+  /// 网络联通测试主机地址
+  public static var reachableTestHost = "https://www.google.com"
+  
+  private static var session: Session?
+  
+  public static var moyaSession: Session {
+    return defaultSession()
+  }
+  
+  private static func defaultSession() -> Session {
+    if let session = self.session {
+      return session
+    }
+    let configuration = URLSessionConfiguration.default
+    configuration.headers = .default
+    let new = Session(configuration: configuration, startRequestsImmediately: false)
+    self.session = new
+    return new
+  }
+  
+  private init() {}
+  
+  
+  
+  /// 发起网络请求
+  /// - api: 业务层自定义网络请求API
+  /// - enableLog: 针对当前请求的日志是否启用
+  /// - CachableTarget: 为业务层API扩展了缓存能力
+  /// - completion: 回调结果
+  @discardableResult
+  public static func commonRequest<API>(
+    api: API,
+    enableLog: Bool = false,
+    progress: ((Double) -> Void)? = nil,
+    completion: @escaping NetworkCallback<Response>
+  ) -> Cancellable?
+  where API: CachableTarget {
     
-    /// 需要捕获的HTTP错误码列表，比如: [404,401]
-    public static var globalCatchHttpCodeList: [Int] = []
-    
-    /// 设置全局捕获HTTP错误码的动作， 闭包参数为：( API, 状态码，继续执行的回调: 是否将任务标记为取消)
-    public static var globalCatchHttpErrorCodeAction: ((CachableTarget, Int, @escaping NetworkCallback<Response>, CatchCallback) -> Void)?
-    
-    /// 设置判定领域设计(即服务器接口数据设计)请求成功的设计标记
-    public static var domainSucessKeyValePair: (String, DNDomainCode) = ("code", DNDomainCode(stringValue: "C0000"))
-    
-    public static var domainFailedMessageKey: String = "message"
-    
-    /// 设置全局捕获领域设计(即服务器接口数据设计)错误码的动作，闭包参数为：( API, 响应原始数据，需要继续执行的回调: 是否将任务标记为取消)
-    public static var domainMiddlewareAction: ((CachableTarget, Data, @escaping NetworkCallback<Response>, CatchCallback) -> Void)?
-    
-    /// 是否全局启用日志
-    public static var isEnableLog = false
-    
-    /// 默认请求超时
-    public static var defaultTimeOut: Double = 30
-    
-    /// 网络联通测试主机地址
-    public static var reachableTestHost = "https://www.google.com"
-    
-    private static var session: Session?
-    
-    public static var moyaSession: Session {
-        return defaultSession()
+    guard let reach = try? Reachability(hostname: reachableTestHost),
+          reach.connection != .unavailable else {
+      completion(.failure(.requestError))
+      return nil
     }
     
-    private static func defaultSession() -> Session {
-        if let session = self.session {
-            return session
+    let provider = MoyaProvider<API>(
+      requestClosure: { (endPoint, done) in
+        do {
+          // 配置超时时间、缓存策略
+          var request = try endPoint.urlRequest()
+          request.timeoutInterval = api.timeout ?? defaultTimeOut
+          if let cachePolicy = api.cachePolicy {
+            request.cachePolicy = cachePolicy
+          }
+          done(.success(request))
+        } catch {
+          if isEnableLog && enableLog {
+            logger.log(.error, args: error)
+          }
+          completion(.failure(.requestError))
         }
-        let configuration = URLSessionConfiguration.default
-        configuration.headers = .default
-        let new = Session(configuration: configuration, startRequestsImmediately: false)
-        self.session = new
-        return new
+      },
+      session: defaultSession(),
+      plugins: []
+    )
+    
+    // 获取发送时间
+    let beginTime = Date().timeIntervalSince1970
+    
+    return provider.request(api, callbackQueue: DispatchQueue.main) { resp in
+      progress?(resp.progress)
+    } completion: { result in
+      switch result {
+      case .success(let response):
+        // 获取请求耗时
+        let useTime = Int((Date().timeIntervalSince1970 - beginTime) * 1000)
+        logResponse(api: api, response: response, useTime: useTime, enableLog: enableLog)
+        handleResponse(api: api, response: response, completion: completion)
+      case .failure(let error):
+        
+        // 请求被取消
+        guard case let .underlying(afError as AFError, _) = error,
+              case .explicitlyCancelled = afError else {
+          logError(api: api, error: nil, enableLog: enableLog)
+          return
+        }
+        logError(api: api, error: error, enableLog: enableLog)
+        
+        completion(.failure(.requestError))
+      }
     }
-    
-    private init() {}
-    
-    
-    
-    /// 发起网络请求
-    /// - api: 业务层自定义网络请求API
-    /// - enableLog: 针对当前请求的日志是否启用
-    /// - CachableTarget: 为业务层API扩展了缓存能力
-    /// - completion: 回调结果
-    @discardableResult
-    public static func commonRequest<API>(
-        api: API,
-        enableLog: Bool = false,
-        progress: ((Double) -> Void)? = nil,
-        completion: @escaping NetworkCallback<Response>
-    ) -> Cancellable?
-    where API: CachableTarget {
-        
-        guard let reach = try? Reachability(hostname: reachableTestHost),
-              reach.connection != .unavailable else {
-            completion(.failure(.requestError))
-            return nil
-        }
-        
-        let provider = MoyaProvider<API>(
-            requestClosure: { (endPoint, done) in
-                do {
-                    // 配置超时时间、缓存策略
-                    var request = try endPoint.urlRequest()
-                    request.timeoutInterval = api.timeout ?? defaultTimeOut
-                    if let cachePolicy = api.cachePolicy {
-                        request.cachePolicy = cachePolicy
-                    }
-                    done(.success(request))
-                } catch {
-                    if isEnableLog && enableLog {
-                        logger.log(.error, args: error)
-                    }
-                    completion(.failure(.requestError))
-                }
-            },
-            session: defaultSession(),
-            plugins: []
-        )
-        
-        // 获取发送时间
-        let beginTime = Date().timeIntervalSince1970
-        
-        return provider.request(api, callbackQueue: DispatchQueue.main) { resp in
-            progress?(resp.progress)
-        } completion: { result in
-            switch result {
-            case .success(let response):
-                // 获取请求耗时
-                let useTime = Int((Date().timeIntervalSince1970 - beginTime) * 1000)
-                logResponse(api: api, response: response, useTime: useTime, enableLog: enableLog)
-                handleResponse(api: api, response: response, completion: completion)
-            case .failure(let error):
-                
-                // 请求被取消
-                guard case let .underlying(afError as AFError, _) = error,
-                      case .explicitlyCancelled = afError else {
-                    logError(api: api, error: nil, enableLog: enableLog)
-                    return
-                }
-                logError(api: api, error: error, enableLog: enableLog)
-                
-                completion(.failure(.requestError))
-            }
-        }
-    }
+  }
 }
 
 // MARK: - 处理成功
 extension Network {
-    private static func handleResponse<API>(api: API,
-                                            response: Response,
-                                            completion: @escaping NetworkCallback<Response>)
-    where API: CachableTarget {
-        
-        let code = response.statusCode
-        if code != 200 {
-            guard globalCatchHttpCodeList.contains(code),
-                  let catchHandler = globalCatchHttpErrorCodeAction else {
-                completion(.failure(.init(code: .init(intValue: response.statusCode), message: response.description)))
-                return
-            }
-            
-            catchHandler(api, code, completion) {
-                action in
-                switch action {
-                case .markCancel:
-                    completion(.failure(.cancel))
-                case .continue:
-                    completion(.failure(.init(code: .init(intValue: code), message: response.description)))
-                case .transferred:
-                    break
-                }
-            }
-            return
+  private static func handleResponse<API>(api: API,
+                                          response: Response,
+                                          completion: @escaping NetworkCallback<Response>)
+  where API: CachableTarget {
+    
+    let code = response.statusCode
+    if code != 200 {
+      guard globalCatchHttpCodeList.contains(code),
+            let catchHandler = globalCatchHttpErrorCodeAction else {
+        completion(.failure(.init(code: .init(intValue: response.statusCode), message: response.description)))
+        return
+      }
+      
+      catchHandler(api, code, completion) {
+        action in
+        switch action {
+        case .markCancel:
+          completion(.failure(.cancel))
+        case .continue:
+          completion(.failure(.init(code: .init(intValue: code), message: response.description)))
+        case .transferred:
+          break
         }
-        
-        
-        // 全局捕获
-        guard let domainCodeCatchAction = domainMiddlewareAction else {
-            completion(.success(response))
-            return
-        }
-        
-        domainCodeCatchAction(api, response.data, completion) {
-            action in
-            switch action {
-            case .markCancel:
-                completion(.failure(.cancel))
-            case .continue:
-                completion(.success(response))
-            case .transferred:
-                break
-            }
-        }
+      }
+      return
     }
+    
+    
+    // 全局捕获
+    guard let domainCodeCatchAction = domainMiddlewareAction else {
+      completion(.success(response))
+      return
+    }
+    
+    domainCodeCatchAction(api, response.data, completion) {
+      action in
+      switch action {
+      case .markCancel:
+        completion(.failure(.cancel))
+      case .continue:
+        completion(.success(response))
+      case .transferred:
+        break
+      }
+    }
+  }
 }
 
 // MARK: - 日志打印
 extension Network {
-    private static func logResponse<API>(api: API, response: Response, useTime: Int?, enableLog: Bool)
-    where API: CachableTarget {
-        
-        guard isEnableLog && enableLog else { return }
-        
-        let method = api.method.rawValue
-        let url = api.baseURL.absoluteString.appending(api.path)
-        var paramStr = ""
-        if api.method.rawValue == "GET" {
-            paramStr = "\(response.request?.url?.parametersFromQueryString ?? [:])"
-        } else {
-            paramStr = "\(String(data: response.request?.httpBody ?? Data(), encoding: String.Encoding.utf8) ?? "")"
-        }
-        
-        let time = "\(useTime ?? 0) ms"
-        let res = String(data: response.data, encoding: .utf8) ?? ""
-        logger.log(.verbose, args: "⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️")
-        logger.log(.verbose, args: "\(method): \(url) ( \(time) )")
-        logger.log(.verbose, args: "请求参数： \n\(paramStr)")
-        logger.log(.verbose, args: "响应参数：\n\(res)")
-        logger.log(.verbose, args: "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️\n")
+  private static func logResponse<API>(api: API, response: Response, useTime: Int?, enableLog: Bool)
+  where API: CachableTarget {
+    
+    guard isEnableLog && enableLog else { return }
+    
+    let method = api.method.rawValue
+    let url = api.baseURL.absoluteString.appending(api.path)
+    var paramStr = ""
+    if api.method.rawValue == "GET" {
+      paramStr = "\(response.request?.url?.parametersFromQueryString ?? [:])"
+    } else {
+      paramStr = "\(String(data: response.request?.httpBody ?? Data(), encoding: String.Encoding.utf8) ?? "")"
     }
     
-    private static func logError<API>(api: API, error: Error?, enableLog: Bool)
-    where API: CachableTarget {
-        guard isEnableLog && enableLog else { return }
-        let method = api.method.rawValue
-        let url = api.baseURL.absoluteString.appending(api.path)
-        if let err = error {
-            logger.log(.error, args: "请求失败 \(method): \(url)")
-            logger.log(.error, args: "失败原因：\(err.localizedDescription)")
-        } else {
-            logger.log(.warn, args: "取消请求 \(method): \(url) ")
-        }
+    let time = "\(useTime ?? 0) ms"
+    let res = String(data: response.data, encoding: .utf8) ?? ""
+    logger.log(.verbose, args: "⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️")
+    logger.log(.verbose, args: "\(method): \(url) ( \(time) )")
+    logger.log(.verbose, args: "请求参数： \n\(paramStr)")
+    logger.log(.verbose, args: "响应参数：\n\(res)")
+    logger.log(.verbose, args: "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️\n")
+  }
+  
+  private static func logError<API>(api: API, error: Error?, enableLog: Bool)
+  where API: CachableTarget {
+    guard isEnableLog && enableLog else { return }
+    let method = api.method.rawValue
+    let url = api.baseURL.absoluteString.appending(api.path)
+    if let err = error {
+      logger.log(.error, args: "请求失败 \(method): \(url)")
+      logger.log(.error, args: "失败原因：\(err.localizedDescription)")
+    } else {
+      logger.log(.warn, args: "取消请求 \(method): \(url) ")
     }
+  }
 }
 
 
 extension URL {
-    fileprivate var parametersFromQueryString: [String: String]? {
-        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
-              let queryItems = components.queryItems else { return nil }
-        return queryItems.reduce(into: [String: String]()) { (result, item) in
-            result[item.name] = item.value
-        }
+  fileprivate var parametersFromQueryString: [String: String]? {
+    guard let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+          let queryItems = components.queryItems else { return nil }
+    return queryItems.reduce(into: [String: String]()) { (result, item) in
+      result[item.name] = item.value
     }
+  }
 }
--- Tests/DiduNetTests/DiduNetTests.swift
+++ Tests/DiduNetTests/DiduNetTests.swift
@@ -3,43 +3,43 @@ import XCTest
 import RxSwift
 
 enum Api {
-    case test1
-    case test2
-    case test3
-    case token
+  case test1
+  case test2
+  case test3
+  case token
 }
 
 extension Api: CachableTarget {
-    var baseURL: URL {
-        URL(string: "http://192.168.194.52:8000")!
+  var baseURL: URL {
+    URL(string: "http://192.168.194.52:8000")!
+  }
+  
+  var path: String {
+    switch self {
+    case .test1:
+      return "/test1.json"
+    case .test2:
+      return "/test2.json"
+    case .test3:
+      return "/test3.json"
+    case .token:
+      return "/token.json"
     }
-    
-    var path: String {
-        switch self {
-        case .test1:
-            return "/test1.json"
-        case .test2:
-            return "/test2.json"
-        case .test3:
-            return "/test3.json"
-        case .token:
-            return "/token.json"
-        }
-    }
-    
-    var method: Moya.Method {
-        return .get
-    }
-    
-    var task: Moya.Task {
-        return .requestPlain
-    }
-    
-    var headers: [String : String]? {
-        return [:]
-    }
-    
-    
+  }
+  
+  var method: Moya.Method {
+    return .get
+  }
+  
+  var task: Moya.Task {
+    return .requestPlain
+  }
+  
+  var headers: [String : String]? {
+    return [:]
+  }
+  
+  
 }
 
 //extension Api: CachableTarget {
@@ -62,110 +62,110 @@ extension Api: CachableTarget {
 //}
 
 class RefreshTokenManager {
-    static let `default` = RefreshTokenManager()
-    
-    private var isRefreshing: Bool = false
-    
-    private var queue = DispatchQueue(label: "com.xxx.refreshToken")
-    
-    private var list: [(CachableTarget, NetworkCallback<Response>)] = []
-    
-    
-    
-    func checkRefreshingToken(api: CachableTarget, callback: @escaping NetworkCallback<Response> ) {
-        queue.async {
-            self.list.append((api, callback))
-            
-            if !self.isRefreshing {
-                self.requestRefreshToken()
-            }
-        }
+  static let `default` = RefreshTokenManager()
+  
+  private var isRefreshing: Bool = false
+  
+  private var queue = DispatchQueue(label: "com.xxx.refreshToken")
+  
+  private var list: [(CachableTarget, NetworkCallback<Response>)] = []
+  
+  
+  
+  func checkRefreshingToken(api: CachableTarget, callback: @escaping NetworkCallback<Response> ) {
+    queue.async {
+      self.list.append((api, callback))
+      
+      if !self.isRefreshing {
+        self.requestRefreshToken()
+      }
     }
-    
-    
-    private func requestRefreshToken() {
-        self.isRefreshing = true
-        _Concurrency.Task {
-            let tokenResult = await DiduNet.Network.request(api: Api.test3, forType: String.self)
-            
-            switch tokenResult {
-            case .success(let token):
-                // TODO: 保存新token
-                print("---- \(token)")
-                self.queue.async {
-                    self.isRefreshing = false
-                    for it in self.list {
-                        Network.commonRequest(api: Api.test3, completion: it.1)
-                    }
-                    self.list.removeAll(keepingCapacity: true)
-                }
-            case .failure(let error):
-                // TODO: 刷新token失败
-                print(error)
-                break
-            }
-            
+  }
+  
+  
+  private func requestRefreshToken() {
+    self.isRefreshing = true
+    _Concurrency.Task {
+      let tokenResult = await DiduNet.Network.request(api: Api.test3, forType: String.self)
+      
+      switch tokenResult {
+      case .success(let token):
+        // TODO: 保存新token
+        print("---- \(token)")
+        self.queue.async {
+          self.isRefreshing = false
+          for it in self.list {
+            Network.commonRequest(api: Api.test3, completion: it.1)
+          }
+          self.list.removeAll(keepingCapacity: true)
         }
+      case .failure(let error):
+        // TODO: 刷新token失败
+        print(error)
+        break
+      }
+      
     }
-    
-    
-    
+  }
+  
+  
+  
 }
 
 
 final class DiduNetTests: XCTestCase {
-    func testExample() throws {
-        
-    }
+  func testExample() throws {
     
-    func testRefreshTokenRequest() async {
-        Network.domainMiddlewareAction =  {
-            (api, data, comp, callback) in
-            do {
-                let dict = try JSONSerialization.jsonObject(with: data) as? [String: Any]
-                let isRefreshTokenCode = dict?["status"] as? String == "E0001"
-                if isRefreshTokenCode && api.path != Api.token.path {
-                    callback(.transferred)
-                    RefreshTokenManager.default.checkRefreshingToken(api: api, callback: comp)
-                    return
-                } else {
-                    callback(.continue)
-                }
-                
-            } catch {
-                callback(.continue)
-            }
+  }
+  
+  func testRefreshTokenRequest() async {
+    Network.domainMiddlewareAction =  {
+      (api, data, comp, callback) in
+      do {
+        let dict = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        let isRefreshTokenCode = dict?["status"] as? String == "E0001"
+        if isRefreshTokenCode && api.path != Api.token.path {
+          callback(.transferred)
+          RefreshTokenManager.default.checkRefreshingToken(api: api, callback: comp)
+          return
+        } else {
+          callback(.continue)
         }
         
-        
-        async let t1 = Network.request(api: Api.test1, forType: String.self)
-        async let t2 = Network.request(api: Api.test2, forType: String.self)
-        
-        let c = await (t1,t2)
-        print("========================")
-        print(c)
-        print("++++++++++++++++++++++++")
+      } catch {
+        callback(.continue)
+      }
     }
     
-    func testDefaultValue() {
-        struct T: Codable {
-            struct UserInfo: Codable, CustomDefaultValue {
-                static var defaultValue = T.UserInfo()
-                typealias Value = Self
-                
-                @Empty var id: String
-                @Empty var list: [String]
-                @Zero var age: Int
-                @Zero var score: Double
-                
-            }
-            
-            @Empty var desc: String
-            @False var isVip: Bool
-            @CustomValue var userInfo: UserInfo
-        }
+    
+    async let t1 = Network.request(api: Api.test1, forType: String.self)
+    async let t2 = Network.request(api: Api.test2, forType: String.self)
+    
+    let c = await (t1,t2)
+    print("========================")
+    print(c)
+    print("++++++++++++++++++++++++")
+  }
+  
+  func testDefaultValue() {
+    struct T: Codable {
+      struct UserInfo: Codable, CustomDefaultValue {
+        static var defaultValue = T.UserInfo()
+        typealias Value = Self
         
-        let json = """
+        @Empty var id: String
+        @Empty var list: [String]
+        @Zero var age: Int
+        @Zero var score: Double
+        
+      }
+      
+      @Empty var desc: String
+      @False var isVip: Bool
+      @CustomValue var userInfo: UserInfo
+    }
+    
+    let json = """
     {
     "desc": null,
     "isVip": true,
@@ -175,12 +175,12 @@ final class DiduNetTests: XCTestCase {
     }
     }
     """
-        let data = json.data(using: .utf8)!
-        do {
-            let model = try JSONDecoder().decode(T.self, from: data)
-            print(model)
-        } catch {
-            XCTFail("\(error)")
-        }
+    let data = json.data(using: .utf8)!
+    do {
+      let model = try JSONDecoder().decode(T.self, from: data)
+      print(model)
+    } catch {
+      XCTFail("\(error)")
     }
+  }
 }
